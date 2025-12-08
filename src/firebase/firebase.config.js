4a0eaddb05cdba63040d588f0f8cd1371df86ed1
majfujamoury <mahfujaakther186630gmail.com>
2025-12-08 23:28:48 +0600

    update

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MESSAGINGSENDERID
 };
 
 const app = initializeApp(firebaseConfig);
